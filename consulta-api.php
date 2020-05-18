9a1b3a985fd3afe05b3835a31de572350d4ca241
Alejandro Alvarez luque <65513462+alejoluque@users.noreply.github.com>
2020-05-18 16:16:01 -0500

    Update consulta-api.php

--- consulta-api.php
+++ consulta-api.php
@@ -14,11 +14,7 @@ function get_posts_general() {
 	// Exit if nothing is returned.
 	if ( empty( $posts ) ) {
 		return;
-	}
-		?>
-		<div class="container" id="recent-post-home">
-		<div class="row">
-		<?php
+	}		
 	// Si hay publicaciones.
 	if ( ! empty( $posts ) ) {
 		// For each post.
@@ -34,23 +30,18 @@ function get_posts_general() {
 			$cat_post = wp_remote_get( 'https://www.dominio.com/wp-json/wp/v2/categories/'.$cat  );
 			$posts_cat = json_decode( wp_remote_retrieve_body( $cat_post ) );
 			// Mostrar título vinculado y una fecha de publicación.
-			$allposts .= '<div class="mk-post-box col-12 col-md-4">';
-			$allposts .= '<div class="mk-post-recent">';
+			
 			$allposts .= '<a href="' . esc_url( $post->link ) . '" target=\"_blank\"> ';
-			$allposts .= '<div class="mk-feature-image-pr"><img class="mk-feature-image-post-recent" src="'.esc_html( $posts_img ->source_url ).'" alt="'. esc_html( $post->title->rendered ) . '"></div>';
-			$allposts .= '<div class="mk-text-post-recent"><span class="mk-category-post-recent">'.esc_html( $posts_cat ->slug ).'</span><br><br><h3 class="mk-title-post-recent">' . esc_html( $post->title->rendered ) . '</h3></div>';
+			$allposts .= '<img src="'.esc_html( $posts_img ->source_url ).'" alt="'. esc_html( $post->title->rendered ) . '">';
+			$allposts .= ''.esc_html( $posts_cat ->slug ).'<h3>' . esc_html( $post->title->rendered ) . '</h3>';
 			
 			
 			$allposts .= '</a>';
-			$allposts .= '</div>';
-			$allposts .= '</div>';
 			
 		}
 		return $allposts;
 	}
-	?>
-		</div>
-		</div>
+	?>	
 		
 	<?php
 }
